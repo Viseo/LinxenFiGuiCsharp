cda3a64cc26c91dd1856d0924e1dd8ab3a3b0adb
Laupretre Nans <nans.laupretre@gmail.com>
2019-03-13 17:59:02 +0100

    Modify Test project type

---
+++ Sources/Linxens.Tests/DataFileServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Linxens.Tests
+{
+    [TestClass]
+    public class DataFileServiceTest
+    {
+        [TestMethod]
+        public void ReadFileTest()
+        {
+            // TODO
+        }
+    }
+}
---
+++ Sources/Linxens.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Linxens.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Linxens.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("209b88d5-057f-4bd0-b927-93adf2a181c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
