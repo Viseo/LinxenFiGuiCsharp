4e494fa497cbc18c98f2e830801bd0f3120afdf4
LambertLoyeMondjo <LambertLoyeMondjo@DESKTOP-9K1CF7A>
2019-03-14 18:03:19 +0100

    add some changes about submit button

--- Sources/Linxens.Core/Service/DataFileService.cs
+++ Sources/Linxens.Core/Service/DataFileService.cs
@@ -115,12 +115,12 @@ namespace Linxens.Core.Service
         {
        
             File.Move(Path.Combine(RootWorkingPath, WorkingType.RUNNING.ToString(), "runningFile.txt"), Path.Combine(RootWorkingPath, WorkingType.DONE.ToString(), "runningFileDone.txt")); 
-            File.Delete(Path.Combine(RootWorkingPath, WorkingType.TODO.ToString(), "runningFileDone.txt"));
+            File.Delete(Path.Combine(RootWorkingPath, WorkingType.TODO.ToString(), "runningFile.txt"));
         }
 
         public void ErrorFile()
         {
-            File.Move(Path.Combine(RootWorkingPath, WorkingType.RUNNING.ToString(), "RunningReel.txt"), Path.Combine(RootWorkingPath, WorkingType.DONE.ToString(), "runningFileError.txt"));
+            File.Move(Path.Combine(RootWorkingPath, WorkingType.RUNNING.ToString(), "runningFile.txt"), Path.Combine(RootWorkingPath, WorkingType.DONE.ToString(), "runningFileError.txt"));
         }
         private void LoadFileToProcess()
         {
